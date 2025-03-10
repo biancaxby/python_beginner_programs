ebf2dfdf275a94a2ba2a2007ff0d066d8563c181
biancaxby <paulabiancapiquero@gmail.com>
2025-03-10 20:41:32 +0800

    added user input and condition

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,1 +1,12 @@
 #print smaller number
+
+#input two numbers
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+#condition
+if num_1 < num_2:
+    print(f'{num_1} is less than {num_2}')
+
+else:
+    print(f'{num_2} is less than {num_1}')
