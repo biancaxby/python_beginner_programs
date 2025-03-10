8b46bac1c14a9843461f0f81d50feb8a8e5eb4e5
biancaxby <paulabiancapiquero@gmail.com>
2025-03-10 21:06:21 +0800

    added condition

--- print_not_equal.py
+++ print_not_equal.py
@@ -4,4 +4,8 @@
 num_1 = int(input('Enter a number: '))
 num_2 = int(input('Enter a number: '))
 
-#
+#print "not equal" if both numbers are not equal
+if num_1 > num_2 or num_1 < num_2:
+    print('Not equal')
+
+
