b288fb7da01b2eaadac99f0a335afcaefd5130f7
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 21:08:11 +0800

    working as intended

--- first_number_raised_to_second_number.py
+++ first_number_raised_to_second_number.py
@@ -1,1 +1,9 @@
 #print the result when the first number is raised by second number
+
+#ask user for two numbers
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Raise to?: "))
+
+#first number should be raised to the second number
+exponent = num_1 ** num_2
+print(exponent)
