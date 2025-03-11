d93c0ae1fc17c9a58cb982aeaf29e25235752a2e
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 15:33:18 +0800

    first commit

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,1 +1,11 @@
 #2 nums quotient no decimal
+
+#ask user to input rwo numbers
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+#divides value
+quotient = int(num_1 / num_2)
+
+#prints the quotient of the values without the decimal
+print(quotient)
