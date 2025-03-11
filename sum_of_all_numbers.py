886e9ba68d57ab6ad695e07c46edd8435d2d8ad6
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 21:42:55 +0800

    not working as intended yet

--- sum_of_all_numbers.py
+++ sum_of_all_numbers.py
@@ -3,4 +3,7 @@
 #ask user to input 10 numbers
 for i in range (10):
     i += 1
-    print(int(input(f"Enter {i} number: ")))
+    numbers = int(input(f"Enter {i} number: "))
+    
+print(numbers)
+
