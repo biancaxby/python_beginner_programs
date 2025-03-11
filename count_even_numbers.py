9abdd6bbe66260082432fe79f889715f3c68cd99
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 16:27:29 +0800

    first commit

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -1,1 +1,11 @@
 #how many even numbers 
+
+#initialize the value of the counter
+even_count = 0
+
+#even_count will increase every time an odd number is inputed
+for i in range(10):
+    if i % 2 != 0:
+        even_count += 1 
+
+print(f' You have entered {even_count} odd numbers')
