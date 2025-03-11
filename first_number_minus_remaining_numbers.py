b8676b7d42edf3c92b91737c5fbb2d403825df05
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 16:13:23 +0800

    first commit

--- first_number_minus_remaining_numbers.py
+++ first_number_minus_remaining_numbers.py
@@ -1,1 +1,15 @@
 #first number minus the remaining numbers
+
+#ask user for a number
+num_1 = int(input('Enter a number: '))
+
+#for loop will ask user to add more numbers
+for i in range(9):
+    numbers = int(input('Enter a number: '))
+
+sums =  i + numbers
+  
+difference =  sums - num_1
+
+
+print(difference)
