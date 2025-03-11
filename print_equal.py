0ce9273ea3abfe138981a26c85f4b0c9b2af5701
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 17:14:55 +0800

    first commit

--- print_equal.py
+++ print_equal.py
@@ -1,1 +1,12 @@
 #print equal
+
+#ask user for two numbers
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+#set condition
+if num_1 == num_2:
+    print('Equal.')
+
+else:
+    print('Not equal.')
