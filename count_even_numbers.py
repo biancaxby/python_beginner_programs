accf73219ad3a0d7e2709650dd1113be87bf04c4
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 16:29:43 +0800

    added user interface

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -5,6 +5,7 @@ even_count = 0
 
 #even_count will increase every time an odd number is inputed
 for i in range(10):
+    i = int(input('Enter a number: '))
     if i % 2 != 0:
         even_count += 1 
 
