23d748413c57072704d9c59a74db33efd9ecc958
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 22:06:19 +0800

    first commit

--- count_odd_numbers.py
+++ count_odd_numbers.py
@@ -1,1 +1,13 @@
 #print how many odd numbers
+
+#initialize odd counter
+odd_counter = 0
+
+#if print number if dividing it to two will not result to zero
+for i in range(10):
+    number = int(input('Enter a number: '))
+    if number % 2 != 0:
+        odd_counter += 1
+
+
+print(odd_counter)
