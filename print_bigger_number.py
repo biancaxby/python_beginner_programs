800dad8300efb84b0e8b8ecec561551a1667991f
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 17:08:49 +0800

    first commit

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,1 +1,13 @@
 #print bigger number
+
+#ask user to input two numbers
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+#set condition
+if num_1 > num_2:
+    print(f'{num_1} is greater than {num_2}')
+
+else:
+    print(f'{num_2} is greater than {num_1}')
+    
