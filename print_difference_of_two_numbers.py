9b512980726a44caf22d79e1717d498bb6dd8309
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 15:27:01 +0800

    added user input, swapping values and operation

--- print_difference_of_two_numbers.py
+++ print_difference_of_two_numbers.py
@@ -1,1 +1,17 @@
 #2 nums print difference 
+
+#ask user to input rwo numbers
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+#swap values if the second number is bigger so that the answer is not in negative
+if num_1 <num_2:
+    num_1, num_2 = num_2, num_1
+
+#print the difference of two numbers
+diff = num_1 - num_2
+print(diff)
+
+
+
+
