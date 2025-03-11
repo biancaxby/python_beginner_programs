868171b4ba20471bd32a1c5d646eba91e32ecef4
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 18:03:17 +0800

    working as intented

--- quotient_of_two_numbers_with_decimal_point.py
+++ quotient_of_two_numbers_with_decimal_point.py
@@ -1,1 +1,9 @@
 #print quotient of two numbers
+
+#ask user for two numbers
+num_1 = float(input('Enter a number: '))
+num_2 =float(input('Enter a number: '))
+
+#operation
+quotient = num_1 / num_2 
+print(quotient)
