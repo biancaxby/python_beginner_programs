ad729ae5196f48227070bbe1287491ba1f3cd911
biancaxby <paulabiancapiquero@gmail.com>
2025-03-11 20:38:07 +0800

    it finally worked

--- quotient_of_two_numbers_with_decimal_point.py
+++ quotient_of_two_numbers_with_decimal_point.py
@@ -6,6 +6,5 @@ num_2 =float(input('Enter a number: '))
 
 #operation
 quotient = num_1 / num_2 
-decimal_point = '.2f'
-final_answer = f"{quotient,:{decimal_point}}"
+final_answer = f"{quotient:.2f}"
 print(final_answer)
