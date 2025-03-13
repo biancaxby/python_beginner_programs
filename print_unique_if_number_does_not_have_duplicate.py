a6f82c938f57a64ba4885edc95da07cea0e72946
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 17:12:10 +0800

    used list to sort out the duplicates and not duplicates

--- print_unique_if_number_does_not_have_duplicate.py
+++ print_unique_if_number_does_not_have_duplicate.py
@@ -1,1 +1,24 @@
 #print unique when entry does not have a duplicate
+#  Display the number with the most number of duplicate.
+# count numbers
+# separate numbers with duplicates and those without 
+
+initial_list = []
+duplicate = []
+no_duplicate = []
+while True:
+    try:
+        user_input = int(input('Enter a number: '))
+        initial_list.append(user_input)
+        if user_input in initial_list:
+            duplicate.append(user_input)
+        if user_input not in duplicate:
+            no_duplicate.append(user_input)
+    except ValueError:
+        print(no_duplicate)
+
+        
+
+
+
+  
