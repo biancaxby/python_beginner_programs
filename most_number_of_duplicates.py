22aaedcc35e72e0950520784b16884d864e1ec8b
biancaxby <paulabiancapiquero@gmail.com>
2025-03-14 00:21:43 +0800

    used max and keys function to go through values and choose the ones that appeared the most

--- most_number_of_duplicates.py
+++ most_number_of_duplicates.py
@@ -1,1 +1,24 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+
+
+no_duplicate = []
+duplicate = []
+
+while True:
+    try:
+        user_input = int(input('Enter a number: '))
+        if user_input in no_duplicate:
+            duplicate.append(user_input)
+        else:
+            no_duplicate.append(user_input)
+
+        
+    except ValueError:
+        if duplicate:
+            duplicates = max(set(duplicate), key=duplicate.count)
+            print(f'The most duplicate numbers is {duplicates}')
+        
+
+
+    
