2f5e17ab57df5388429218044d05048d3305ed94
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 20:29:02 +0800

    used sort and append in a while loop

--- display_the_lowest_number.py
+++ display_the_lowest_number.py
@@ -1,1 +1,18 @@
 #display the lowest number
+
+#sort
+#print index 1
+
+number_list = []
+
+while True:
+    try:
+        user_input = int(input('Enteer a number: '))
+        number_list.append(user_input)
+        
+    except:
+        print('error')
+        break
+        
+h = number_list.sort
+print(h[1])
