df9bc328f0aea6062e92d2a52339b80149506c26
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 14:59:42 +0800

    used 2 lists to append and printed only of the list

--- print_only_first_entry_of_duplicate_number.py
+++ print_only_first_entry_of_duplicate_number.py
@@ -1,18 +1,20 @@
 #display all numbers but for numbers with duplicates, only the first entry should be printed
 
+
+# add two list
 users = []
 users_2 = []
+
+# all the values that were added should be appended to users list. but if the value is already there, it should be appended to users_2
+#since they are separated, i can only print one of the lists
+
 for i in range (5):
     user_input = int(input('Enter a number: '))
     if user_input in users:
         users_2.append(user_input)
     else:
         users.append(user_input)
-     
-
-
-
-
+    
 
 print(users)
 
