1d8028e7725e690fd8599ac8fa87c1b0af174964
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 23:11:32 +0800

    deleted the files due to a mistake

--- display_highest_number.py
+++
@@ -1,1 +0,0 @@
-#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
--- display_numbers_that_have_duplicate.py
+++
@@ -1,1 +0,0 @@
-#Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
