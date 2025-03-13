ede4c487f23a3cafc90428dd0445c62c4a5dd153
biancaxby <paulabiancapiquero@gmail.com>
2025-03-14 01:21:40 +0800

    used funtions lists, sum and len to determine the average of all input numbers

--- display_the_average_of_input.py
+++ display_the_average_of_input.py
@@ -1,1 +1,19 @@
 #Display the average
+
+number_list = []
+
+while True:
+    try:
+        numbers = int(input('Enter a number: '))
+        number_list.append(numbers)
+    
+    except ValueError:
+        print('Error')
+        break
+
+
+average = sum(number_list) / len(number_list)
+print(average)
+
+
+
