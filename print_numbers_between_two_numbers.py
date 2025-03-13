fa39b6b3fdb87f3ce2b94ece3fcc54da9316df9b
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 22:04:22 +0800

    fixed an error. it prints equal despite not being equal

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -12,7 +12,7 @@ if num_1 < num_2:
 elif num_1 > num_2:
     for i in range(num_1 - 1, num_2, -1):       # reversed the order
             number_between.append(i)
-    else:
+elif num_1 == num_2:
         print('The numbers are equal.')
 
 print(number_between)
