54e8ab2ba4d59f1ed2b9e0d328e7e71215ed0b24
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 14:26:40 +0800

    try

--- print_only_first_entry_of_duplicate_number.py
+++ print_only_first_entry_of_duplicate_number.py
@@ -1,1 +1,13 @@
 #display all numbers but for numbers with duplicates, only the first entry should be printed
+
+users = []
+
+for i in range (5):
+    user_input = input('Enter a number: ')
+    users.append(user_input)
+
+new_list = [num for num in users if users.count < 1]
+
+print(new_list)
+
+
