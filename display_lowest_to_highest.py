d4043ba6ca03bffdaa4d7d92e9aef41875ada753
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 21:38:21 +0800

    used while loop so that i can use the break function

--- display_lowest_to_highest.py
+++ display_lowest_to_highest.py
@@ -1,1 +1,16 @@
 #lowest to highest number
+
+# initialize list
+number_list = []
+
+while True:
+    try:
+        user_input = int(input('Enter a number: '))
+        number_list.append(user_input) # adds the numbers in the list
+        number_list.sort()  # sorts out the value
+    except:
+        print('Error. Invalid input')   
+        break
+        
+
+print(number_list)
--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -1,1 +1,19 @@
 #print all numbers between the two numbers
+
+
+
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+while True:
+    if num_1 < num_2:
+        for i in range(num_1 - num_2):
+            num_1 += 1 
+            if num_1 >= num_2:
+                break
+    if num_1 > num_2:
+        num_1 -= num_2
+        if num_1 <= num_2:
+            break
+
+print(num_1)
