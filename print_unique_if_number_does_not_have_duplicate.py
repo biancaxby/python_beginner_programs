58e08c326cc37c755925432265e6581a01ad5520
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 17:45:33 +0800

    tried remove() to remove the duplicate numbers

--- print_unique_if_number_does_not_have_duplicate.py
+++ print_unique_if_number_does_not_have_duplicate.py
@@ -1,22 +1,21 @@
 #print unique when entry does not have a duplicate
-#  Display the number with the most number of duplicate.
 # count numbers
 # separate numbers with duplicates and those without 
 
-initial_list = []
+
 duplicate = []
 no_duplicate = []
 while True:
     try:
         user_input = int(input('Enter a number: '))
-        initial_list.append(user_input)
-        if user_input in initial_list:
-            duplicate.append(user_input)
-        if user_input not in duplicate:
-            no_duplicate.append(user_input)
+        no_duplicate.append(user_input)
     except ValueError:
-        print(no_duplicate)
+        break
 
+if user_input in no_duplicate:
+    duplicate.append(no_duplicate)
+    
+print(f'These numbers are unique! {no_duplicate}')
         
 
 
