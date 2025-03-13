fcb6414330eedda07a7981662ed84bba63e91b1f
biancaxby <paulabiancapiquero@gmail.com>
2025-03-14 01:40:08 +0800

    used for loop and lists to separate the values with and without duplicates

--- display_numbers_that_have_duplicates.py
+++ display_numbers_that_have_duplicates.py
@@ -1,1 +1,15 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+
+no_duplicates = []
+duplicates = []
+
+for i in range(10):
+        user_input = int(input('Enter a number: '))
+        if user_input in no_duplicates:
+            duplicates.append(user_input)
+        if user_input not in duplicates:    
+            no_duplicates.append(user_input)
+              
+
+print(duplicates)
