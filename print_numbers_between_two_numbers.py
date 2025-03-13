7e3858e8ef4987e082940d549ef5188d2f7d55fe
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 22:01:34 +0800

    replaced the num_1 with i in the append parenthesis

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -8,10 +8,10 @@ num_2 = int(input('Enter a number: '))
 number_between = []
 if num_1 < num_2:
     for i in range(num_1 + 1, num_2):
-            number_between.append(num_1)
+            number_between.append(i)
 elif num_1 > num_2:
     for i in range(num_1 - 1, num_2, -1):       # reversed the order
-            number_between.append(num_1)
+            number_between.append(i)
     else:
         print('The numbers are equal.')
 
