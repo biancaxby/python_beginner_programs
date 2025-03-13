4789a14137237c6725fc25d69baf48d167d58a78
biancaxby <paulabiancapiquero@gmail.com>
2025-03-13 13:55:36 +0800

    tried list comprehension

--- numbers_with_no_duplicate.py
+++ numbers_with_no_duplicate.py
@@ -1,13 +1,12 @@
 #display all numbers that dont have a duplicate
 users = []
 
-for i in range(5):
-    user = input("enter: ")
-    if user in users:
-        user.append(users)
-   
-    if user in users is > 1:
-        users.clear()
+# ask user for numbers
+for i in range(10):
+    user = input("Enter a number: ")
+    users.append(user)
 
-print(users)
-        
+# Filter out numbers that have duplicates
+no_duplicate = [num for num in users if users.count(num) == 1]
+
+print("Numbers without duplicates:", no_duplicate)
