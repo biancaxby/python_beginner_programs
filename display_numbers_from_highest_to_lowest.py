e175b79d134afdebf1223ce2807911f2ec1eaf1e
biancaxby <paulabiancapiquero@gmail.com>
2025-03-14 00:58:37 +0800

    used reverse on sort function to reverse the order of the values to a descending order

--- display_numbers_from_highest_to_lowest.py
+++ display_numbers_from_highest_to_lowest.py
@@ -1,1 +1,17 @@
 #Display the number from highest to lowest
+
+lists = []
+
+while True:
+    try:
+        user_input = int(input('Enter a number: '))
+        
+        if user_input:
+            lists.append(user_input)
+            lists.sort(reverse=True)
+
+    except ValueError:
+        print('Invalid input')
+        break
+
+print(lists)
