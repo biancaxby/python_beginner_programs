1e7ee3120878b5f74d6611cff73bb5386e1843f3
biancaxby <paulabiancapiquero@gmail.com>
2025-03-14 00:51:00 +0800

    used max function to print the highest value in the list

--- display_the_highest_number.py
+++ display_the_highest_number.py
@@ -1,1 +1,14 @@
 #Display the highest number
+
+list = []
+
+while True:
+    try:
+        user_input = int(input('Enter a number: '))
+        if user_input:
+            list.append(user_input)
+    except ValueError:
+        print('Invalid input')
+        break
+
+print(max(list))
